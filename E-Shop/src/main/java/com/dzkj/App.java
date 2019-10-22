9e72c4cb4c3f1ed049494d3508728c19670e9908
陈培 <@DESKTOP-3QIQKAL>
2019-10-22 23:59:37 +0800

    1.0

--- E-Shop/src/main/java/com/dzkj/App.java
+++ E-Shop/src/main/java/com/dzkj/App.java
@@ -4,16 +4,22 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //扫描本包或子包下的注解组件
 @SpringBootApplication
 @ServletComponentScan
-@MapperScan("com.dzkj.mapper")
+@Controller
+//@MapperScan("com.dzkj.mapper")
 public class App {
 	public static void main(String[] args) {
 		//springboot开机
 		SpringApplication.run(App.class, args);
 	}
-	
+	@RequestMapping("/index")
+	public String index() {
+		return "person/index";
+	}
 	
 }
